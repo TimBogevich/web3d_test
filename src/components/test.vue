61adf8a8d62b4d921e2b32f591be61f54ea56a74
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2021-01-15 13:06:25 +0100

    added slider to camera

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <v-slider v-model="value" step="1"></v-slider>
      <canvas id="renderCanvas" style="height: 100%; width: 100%"></canvas>
   </div>
 </template>
@@ -12,6 +13,7 @@
   export default {
     data() {
       return {
+        value : 50,
         scene: null,
         engine: null,
         canvas: null,
@@ -29,7 +31,7 @@
         this.scene = new BABYLON.Scene(this.engine)
         this.scene.clearColor = new BABYLON.Color4(0.8, 0.8, 0.156, 0.9);
 
-        this.mainCamera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1.5, 1.3, 1.6, BABYLON.Vector3.Zero(), this.scene)
+        this.mainCamera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1.5, 1.3, 2, BABYLON.Vector3.Zero(), this.scene)
         this.mainCamera.attachControl(this.canvas, true)
         this.scene.beforeRender = function () {
 
@@ -52,6 +54,14 @@
       console.log("test")
       this.init()
     },
+    watch : {
+      value(newValue, oldValue) {
+        let camera = this.mainCamera
+        newValue = 2 + (newValue - 50) / 10
+        camera.setPosition(new BABYLON.Vector3(1.5, 1.3, newValue));
+        console.log(newValue)
+      }
+    } 
   }
 </script>
 
