398f202cfb1acc8a6933ce9a51678b29b19bb3e1
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2021-01-15 16:29:20 +0100

    fixes

--- src/components/test.vue
+++ src/components/test.vue
@@ -7,6 +7,7 @@
 
 <script>
   import * as BABYLON from 'babylonjs';
+  import * as BABYLON_MAT from "babylonjs-materials";
   import 'babylonjs-loaders';
   import McLaren from 'file-loader!@/assets/mc-laren/source/McLaren.glb';
 
@@ -20,6 +21,7 @@
         canvas: null,
         mainCamera: null,
         light : null,
+        shadowGenerator : null,
         McLaren : McLaren.replace("/", ""),
       };
     },
@@ -33,17 +35,36 @@
         this.engine = new BABYLON.Engine(this.canvas, true)
         this.scene = new BABYLON.Scene(this.engine)
         this.scene.debugLayer.show();
-        this.scene.clearColor = new BABYLON.Color4(0.8, 0.8, 0.156, 0.9);
 
         this.light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(1.5, -2.3, -1.6), this.scene)
-        this.light.intensity = 1;
+        this.light.intensity = 10;
+
+        // this.ground = BABYLON.Mesh.CreateGround("ground", 500, 500, 5, this.scene);
+        // this.ground.receiveShadows = true
+
+        var helper = this.scene.createDefaultEnvironment({
+            enableGroundShadow: true
+        });
+        helper.setMainColor(BABYLON.Color3.Gray());
+        helper.ground.position.y += 0;
 
         this.mainCamera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1.5, 1.3, 2, BABYLON.Vector3.Zero(), this.scene)
 
         this.engine.runRenderLoop(function () {
           _this.scene.render()
         })
+
+        this.shadowGenerator = new BABYLON.ShadowGenerator(512, this.light)
+        this.shadowGenerator.useBlurExponentialShadowMap = true;
+        this.shadowGenerator.blurScale = 2;
+        this.shadowGenerator.setDarkness(0.75);
+
+
         BABYLON.SceneLoader.Append("", this.McLaren, this.scene, (scene) => {
+          let car = scene.getNodeByID("__root__")
+          car.scaling = new BABYLON.Vector3(3, 3, 3);
+          car.position.y = 0.2
+          this.shadowGenerator.addShadowCaster(car);
         });
 
       }
